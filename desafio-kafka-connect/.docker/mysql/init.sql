6e1e9e97494d5d3de5e28047c6a7679097bbed08
Luis Daniel Assulfi <lassulfi@gmail.com>
2021-09-16 22:37:23 -0300

    Iniciado desafio 2

---
+++ desafio-kafka-connect/.docker/mysql/init.sql
@@ -0,0 +1,42 @@
+CREATE DATABASE IF NOT EXISTS fin;
+
+USE fin;
+
+CREATE TABLE IF NOT EXISTS tb_accounts (
+    id VARCHAR(36) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    balance FLOAT NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME ON UPDATE CURRENT_TIMESTAMP
+);
+CREATE TABLE IF NOT EXISTS tb_transactions (
+    id VARCHAR(36) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    category VARCHAR(255) NOT NULL,
+    amount FLOAT NOT NULL,
+    `type` VARCHAR 255 NOT NULL,
+    account_id VARCHAR(36),
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (account_id) REFERENCES tb_accounts(account_id)
+);
+
+INSERT INTO tb accounts VALUES 
+    (`1`, `BRADESCO`, 1000),
+    (`2`, `ITAU`, 1500);
+
+INSERT INTO tb_transactions VALUES
+    (`1`, `Conta de luz`, `Conta de Luz`, `CASA`, -100, `DEBITO`, 1),
+    (`2`, `Salario`, `Salario`, `SALARIO`, 3000, `CREDITO`, 2),
+    (`3`, `Condominio`, `Moradia`, `CASA`, -400, `DEBITO`, 1),
+    (`4`, `Internet`, `Internet`, `CASA`, -100, `DEBITO`, 1),
+    (`5`, `Telefone`, `Telefone`, `CASA`, -40, `DEBITO`, 1),
+    (`6`, `TV por assinatura`, `TV por assinatura`, `CASA`, 70, `DEBITO`, 1),
+    (`7`, `Transferencia BRADESCO`, `Tranferencia Bradesco`, `Finanças`, -2000, `DEBITO`, 2),
+    (`8`, `Credito ITAU`, `Credito ITAU`, `Financas`, 2000, `CREDITO`, 1),
+    (`9`, `Adiantamento 13`, `Adiantamento 13 Salario`, `SALARIO`, 2500, `DEBITO`, 2),
+    (`10`, `Adiantamento Salario`, `Adiantamento Salario`, `SALARIO`, 2000, `DEBITO`, 2),
+    (`11`, `Telefone Celular`, `Telefone Celular`, `TELEFONIA`, -50, `DEBITO`, 1),
+    (`12`, `Supermercado`, `Supermercado`, `CASA`,- 900, `DEBITO`, 1),
+    (`13`, `Alimentacao`, `Alimentacao`, `ALIMENTACAO`, -500, `DEBITO`, 1);
